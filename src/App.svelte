5aa486f3055af6ddf442e23d884e3000d176a92c
April Hall <ari@arithefirst.com>
2024-09-26 01:19:49 -0400

    Add mobile friendliness for TechStack component

--- src/App.svelte
+++ src/App.svelte
@@ -2,26 +2,18 @@
     import "./app.css";
     import Navbar from "./lib/Navbar.svelte";
     import Techstack from "./lib/Techstack.svelte";
-
-    const itemsRowOne = {
+    const items = {
         "arch":"other", "bash":"lang", "bootstrap":"framework",
         "bun":"tool", "css":"lang", "discordjs":"framework",
         "docker":"tool", "git":"tool", "github":"tool",
         "go":"lang", "html":"lang", "javascript":"lang",
-        "jquery":"framework",
-    }
-
-    const itemsRowTwo = {
-        "linux":"other", "mongodb":"other", "neovim":"tool",
+        "jquery":"framework", "linux":"other", "mongodb":"other", "neovim":"tool",
         "nodejs":"tool", "npm":"tool", "python":"lang",
         "svelte":"framework", "tailwind":"framework", "threejs":"framework",
         "typescript":"lang", "vim":"tool", "vscodium":"tool",
-        "webstorm":"tool",
+        "webstorm":"tool"
     }
-
 </script>
 
 <Navbar />
-
-
-<Techstack {itemsRowOne} {itemsRowTwo}/>
+<Techstack {items}/>
--- src/lib/Techstack.svelte
+++ src/lib/Techstack.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
     // See codepen.io/April-Hall/pen/ZEgEGEg
     // for my original concept for this
-    export let itemsRowOne: {[key: string]: string}
-    export let itemsRowTwo: {[key: string]: string}
+    export let items: {[key: string]: string}
 
 
     let mode: number;
@@ -12,51 +11,39 @@
 
 </script>
 
-<h1 class="text-3xl font-bold text-center">Technologies</h1>
+<h1 class="text-3xl font-bold text-center md:mt-0 mt-5">Technologies</h1>
 <hr class="w-1/2 mx-auto my-3 border-base-content">
 
 <div id="techstack">
     <div class="w-fit mx-auto my-2">
         <button
-            class="btn btn-sm btn-primary mx-1"
+            class="btn md:btn-sm btn-xs btn-primary mx-1"
             on:mouseenter={() => {setOffset(1)}}
             on:mouseleave={() => {setOffset(0)}}
         >Tools</button>
-
         <button
-            class="btn btn-sm btn-primary mx-1"
+            class="btn md:btn-sm btn-xs btn-primary mx-1"
             on:mouseenter={() => {setOffset(2)}}
             on:mouseleave={() => {setOffset(0)}}
         >Frameworks & Libs</button>
-
         <button
-            class="btn btn-sm btn-primary mx-1"
+            class="btn md:btn-sm btn-xs btn-primary mx-1"
             on:mouseenter={() => {setOffset(3)}}
             on:mouseleave={() => {setOffset(0)}}
         >Languages</button>
-
         <button
-                class="btn btn-sm btn-primary mx-1"
+                class="btn md:btn-sm btn-xs btn-primary mx-1"
                 on:mouseenter={() => {setOffset(4)}}
                 on:mouseleave={() => {setOffset(0)}}
         >Other</button>
     </div>
 
-    <div class="w-fit h-16 mx-auto relative img-container">
-        {#each Object.entries(itemsRowOne) as [item, type]}
+    <div class="w-11/12 md:w-1/2 h-16 mx-auto relative img-container flex flex-wrap justify-center">
+        {#each Object.entries(items) as [item, type]}
             <img
                 alt={item.charAt(0).toUpperCase() + item.slice(1)}
                 src={"https://skillicons.dev/icons?i=" + item}
-                class="border-2 rounded-[18px] w-16 h-16 inline-block mx-1 {'type-' + type}"
-            >
-        {/each}
-    </div>
-    <div class="w-fit h-16 mx-auto relative img-container">
-        {#each Object.entries(itemsRowTwo) as [item, type]}
-            <img
-                alt={item.charAt(0).toUpperCase() + item.slice(1)}
-                src={"https://skillicons.dev/icons?i=" + item}
-                class="border-2 rounded-[18px] w-16 h-16 inline-block m-1 {'type-' + type}"
+                class="border-2 rounded-[18px] w-16 h-16 inline-block m-0.5 md:m-1 {'type-' + type}"
             >
         {/each}
     </div>

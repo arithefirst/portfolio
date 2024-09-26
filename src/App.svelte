a414ae4e948f936498cbef341fc35e69d08db0f9
April Hall <ari@arithefirst.com>
2024-09-26 13:16:45 -0400

    Create Projectcard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
     import "./app.css";
     import Navbar from "./lib/Navbar.svelte";
     import Techstack from "./lib/Techstack.svelte";
+    import Projectcard from "./lib/Projectcard.svelte";
     const items = {
         "arch":"other", "bash":"lang", "bootstrap":"framework",
         "bun":"tool", "css":"lang", "discordjs":"framework",
@@ -13,7 +14,13 @@
         "typescript":"lang", "vim":"tool", "vscodium":"tool",
         "webstorm":"tool"
     }
+
 </script>
 
 <Navbar />
-<Techstack {items}/>
+
+<Techstack {items} />
+
+<div class="md:grid-cols-4 grid-cols-2 flex flex-wrap w-fit mx-auto">
+    <!--  Add <Projectcard /> Components here  -->
+</div>
---
+++ src/lib/Projectcard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let name: string
+    export let src: string
+    export let alt: string
+    export let href: string
+    export let desc: string
+    export let tags: string[]
+</script>
+
+<div class="card bg-base-100 shadow-xl cursor-pointer mx-4 relative md:w-64 w-48 my-3">
+    <figure>
+        <img {src} {alt} class="w-full"/>
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">{name}</h2>
+        <p>{desc}</p>
+        <div class="card-actions justify-end">
+            {#each tags as tag}
+                <div class="badge badge-outline">{tag}</div>
+            {/each}
+        </div>
+    </div>
+</div>

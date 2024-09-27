845ee287c3ff9ceaac114f165cecf0c5a3da2e9b
April Hall <ari@arithefirst.com>
2024-09-27 03:16:00 -0400

    Add cursor tracking effect to Projectcard component

--- src/lib/Projectcard.svelte
+++ src/lib/Projectcard.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+
+    import { onMount} from "svelte";
+
     export let name: string
     export let src: string
     export let alt: string
@@ -6,14 +9,53 @@
     export let desc: string
     export let tags: string[]
     export let index: number
+
+
+
+    let rotateX: string
+    let rotateY: string
+    let cardW: number
+    let cardH: number
+    let component: Element
+    let componentTop: number
+    let componentLeft: number
+
+    document.addEventListener("mousemove", (e) => {
+        rotateElement(e);
+    });
+
+    // Get the corner of the component once it loads in
+    onMount(() => {componentLeft = component.getBoundingClientRect().left;
+                   componentTop = component.getBoundingClientRect().top})
+
+    // Im gonna be so fr i fucked up some math in here
+    // and idk what's going on in this function anymore
+    function rotateElement(event: MouseEvent) {
+        // Find mouse x and y relative to the container
+        const x = event.clientX - componentLeft;
+        const y = event.clientY - componentTop;
+
+        // Find the offset and tone it down
+        const offsetX = ((x - (cardW / 2)) / (cardW / 2)) * 25;
+
+        // Use a lower offset here bc there is always more
+        // vertical space than horizontal space
+        const offsetY = ((y - (cardH / 2)) / (cardH / 2)) * -15;
+
+        // set rotation
+        rotateY = offsetX + "deg";
+        rotateX =- -1 * offsetY + "deg";
+    }
+
 </script>
 
 <div class="card bg-base-200 cursor-pointer relative grid w-11/12 md:w-64 my-3
      md:hover:scale-[1.025] md:linear md:transition-all"
      on:click={() => {window.location.replace(href);}}
      on:keypress={() => {window.location.replace(href);}}
-     role="button"
-     tabindex={index}
+     role="button" tabindex={index} bind:this={component}
+     bind:clientWidth={cardW} bind:clientHeight={cardH}
+     style="--rotateX: {rotateX}; --rotateY: {rotateY};"
 >
     <figure>
         <img {src} {alt} class="object-cover w-full h-full bg-base-300"/>
@@ -28,3 +70,12 @@
         </div>
     </div>
 </div>
+
+<style>
+    .card:hover {
+        transform:
+            perspective(5000px)
+            rotateY(var(--rotateY))
+            rotateX(var(--rotateX))
+    }
+</style>

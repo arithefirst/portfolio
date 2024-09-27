f7e5bafbcb8dec3acbd8f3615741b9ba3cbd56a1
April Hall <ari@arithefirst.com>
2024-09-27 01:39:41 -0400

    Remove shadows

--- src/lib/Projectcard.svelte
+++ src/lib/Projectcard.svelte
@@ -8,7 +8,7 @@
     export let index: number
 </script>
 
-<div class="card bg-base-200 shadow-xl cursor-pointer relative grid w-11/12 md:w-64 my-3
+<div class="card bg-base-200 cursor-pointer relative grid w-11/12 md:w-64 my-3
      md:hover:scale-[1.025] md:linear md:transition-all"
      on:click={() => {window.location.replace(href);}}
      on:keypress={() => {window.location.replace(href);}}

6a6441e75328dc795aa21b5a302ea93081abe748
April Hall <ari@arithefirst.com>
2024-09-27 21:46:49 -0400

    Make hero text coloring uniform in light mode

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,11 +1,11 @@
 <div class="hero min-h-screen">
     <div class="hero-content text-center">
         <div class="my-auto">
-            <h1 class="w-fit font-bold md:text-[225px] text-[20vw] text-white eight-bit mx-auto">HELLO!</h1>
+            <h1 class="w-fit font-bold md:text-[225px] text-[20vw] text-neutral dark:text-white eight-bit mx-auto">HELLO!</h1>
             <p class="my-3 max-w-md dark:text-white mx-auto font-normal text-4xl">
                 I'm <strong class="font-black">April Hall</strong>
             </p>
-            <hr class="w-3/5 mx-auto border-2 border-black dark:border-white ">
+            <hr class="w-3/5 mx-auto border-2 border-neutral dark:border-white ">
             <p class="my-3 max-w-md dark:text-white mx-auto font-medium text-4xl">
                 Fullstack Developer
             </p>

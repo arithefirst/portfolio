0558909752a95edb6edbe70443c65235978f0cd6
April Hall <ari@arithefirst.com>
2024-09-29 15:16:36 -0400

    Hide dropdown on md incase window is sm and then resized

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -45,7 +45,7 @@
                     </g>
                 </svg>
             </summary>
-            <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <ul class="md:hidden menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                 <li><a href="/">Home</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li><a href="#Projects">Projects</a></li>

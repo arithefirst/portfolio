c79cd5f75774004df114da876aca86d26239900c
April Hall <ari@arithefirst.com>
2024-10-21 11:37:32 -0400

    Switch technologies and projects

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,13 @@
     import Navbar from "./lib/Navbar.svelte";
     import Techstack from "./lib/Techstack.svelte";
     import Projectcard from "./lib/Projectcard.svelte";
-    import { items } from './lib/techItems';
 </script>
 <Navbar />
 
 <Hero />
+
+<Techstack />
+
 <div class="projects" id="projects">
     <h1 class="text-3xl font-bold text-center md:mt-5 mt-5">Projects</h1>
     <hr class="w-1/2 mx-auto my-3 border-base-content">
@@ -41,7 +43,6 @@
     </div>
 </div>
 
-<Techstack />
 <div class="h-8"/>
 
 <style>
--- src/lib/Techstack.svelte
+++ src/lib/Techstack.svelte
@@ -10,7 +10,7 @@
 
 </script>
 
-<h1 class="text-3xl font-bold text-center md:mt-0 mt-5">Technologies</h1>
+<h1 class="text-3xl font-bold text-center mt-5">Technologies</h1>
 <hr class="w-1/2 mx-auto my-3 border-base-content">
 
 <div id="techstack">

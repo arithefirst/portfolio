3a93c3b3d9ff771d9b92b9eaf11637016075d8c0
April Hall <ari@arithefirst.com>
2024-11-06 02:25:18 -0500

    Add PGP Publickey to Contact Page

--- src/lib/ContactElement.svelte
+++ src/lib/ContactElement.svelte
@@ -2,16 +2,18 @@
     export let icon: string;
     export let name: string;
     export let href: string;
+    export let className: string = "";
     import Icon from '@iconify/svelte';
 </script>
 
 <a
-    class="h-8 md:w-48 w-full btn mx-1
-    relative my-1 btn-primary
+    class="h-8 w-full min-w-48 btn mx-1
+    relative my-0.5 btn-primary
     light:text-accent-content
     dark:text-base-content
-    border-transparent shadow-md"
-    {href} target="_blank"
+    border-transparent shadow-md
+    {className}" {href}
+    target="_blank"
 >
     <Icon icon={icon} class="absolute right-5 text-xl"/>
     <p class="absolute text-l left-5">{name}</p>

cfd6e48b6796473cb7bde28cd39ea4702f53c3d4
April Hall <ari@arithefirst.com>
2024-11-13 00:02:41 -0500

    Add lockbox to projects

--- src/App.svelte
+++ src/App.svelte
@@ -22,6 +22,14 @@
   <div class="md:grid-cols-4 grid-cols-1 w-fit mx-auto flex flex-wrap justify-center gap-4">
     <!--  Add <Projectcard /> Components here  -->
     <Projectcard
+      href="https://github.com/arithefirst/lockbox"
+      name="Lockbox"
+      src="/lockbox.svg"
+      alt="valo builder logo"
+      desc="A password protected dropbox where users given a password can upload a file "
+      tags={["DaisyUI", "Docker", "Svelte", "Sveltekit", "Tailwind", "Typescript"]}
+    />
+    <Projectcard
       href="https://github.com/arithefirst/now-playing-widget"
       name="Now Playing Widget"
       src="/now-playing-widget.webp"
@@ -30,20 +38,20 @@
       tags={["Golang", "Javascript", "HTML5", "CSS3"]}
     />
     <Projectcard
-      href="https://github.com/arithefirst/valo-builder"
-      name="Valo Builder"
-      src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
-      alt="valo builder logo"
-      desc="A website for building VALORANT skin collections without sending all of your money to Riot Games"
-      tags={["Golang", "Svelte", "CSS3"]}
-    />
-    <Projectcard
       href="https://github.com/arithefirst/portfolio"
       name="Portfolio"
       src="/pfp.png"
       alt="cat from 2023 steam summer sale"
       desc="My portfolio website, the one you're on now!"
       tags={["DaisyUI", "Svelte", "Tailwind", "Typescript", "Vercel"]}
+    />
+    <Projectcard
+      href="https://github.com/arithefirst/valo-builder"
+      name="Valo Builder"
+      src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+      alt="valo builder logo"
+      desc="A website for building VALORANT skin collections without sending all of your money to Riot Games"
+      tags={["Golang", "Svelte", "CSS3"]}
     />
   </div>
 </div>

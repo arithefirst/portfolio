3cc1dea1ff8f1f76a8054be996a4b3444085ff53
April Hall <ari@arithefirst.com>
2024-11-21 20:52:52 -0500

    Moved images to /static/images/

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -10,15 +10,15 @@
     <ProjectCard
       href="https://github.com/arithefirst/lockbox"
       name="Lockbox"
-      src="/lockbox.svg"
-      alt="valo builder logo"
+      src="/images/lockbox.svg"
+      alt="Lockbox Logo"
       desc="A password protected dropbox where users given a password can upload a file "
       tags={['DaisyUI', 'Docker', 'Svelte', 'Sveltekit', 'Tailwind', 'Typescript']}
     />
     <ProjectCard
       href="https://github.com/arithefirst/now-playing-widget"
       name="Now Playing Widget"
-      src="/now-playing-widget.webp"
+      src="/images/now-playing-widget.webp"
       alt="now playing widget preview"
       desc="A simple widget to show the current song playing on your spotify account. Useful for twitch streams and youtube videos"
       tags={['Golang', 'Javascript', 'HTML5', 'CSS3']}
@@ -26,7 +26,7 @@
     <ProjectCard
       href="https://github.com/arithefirst/portfolio"
       name="Portfolio"
-      src="/pfp.png"
+      src="/images/pfp.png"
       alt="cat from 2023 steam summer sale"
       desc="My portfolio website, the one you're on now!"
       tags={['DaisyUI', 'Svelte', 'Tailwind', 'Typescript', 'Vercel']}

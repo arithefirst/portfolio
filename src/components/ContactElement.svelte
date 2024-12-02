81a066440456f9ee3ffc0f6af58a78945a67a817
April Hall <ari@arithefirst.com>
2024-12-02 10:57:44 -0500

    Store SVGs locally

--- src/components/ContactElement.svelte
+++ src/components/ContactElement.svelte
@@ -1,13 +1,13 @@
 <script>
-const { href, icon, name, className, brand } = $props();
+  const { href, icon, name, className, children } = $props();
 </script>
 
-<a class="h-8 w-full min-w-48 btn relative btn-primary text-base-content border-transparent shadow-md {className}" {href} target="_blank">
-  {#if brand}
-    <img alt="{name} logo" class="absolute right-5 h-5 text-white" src="https://cdn.simpleicons.org/{icon}/white" />
-  {:else}
-    <img alt="{icon} image" class="absolute right-5 h-5 text-white" src="/images/{icon}.svg" />
-  {/if}
+<a
+  class="h-8 w-full min-w-48 btn relative btn-primary text-base-content border-transparent shadow-md {className}"
+  {href}
+  target="_blank"
+>
+  <img alt="{icon} image" class="absolute right-5 h-5 text-white" src="/images/{icon}.svg" />
   <p class="absolute text-l left-5">{name}</p>
 </a>
 

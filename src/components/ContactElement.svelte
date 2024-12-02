70be3bdb5c478eea644cdc522cde3b5f0a1e433f
April Hall <ari@arithefirst.com>
2024-12-02 11:34:02 -0500

    Optimize images with astro

--- src/components/ContactElement.svelte
+++ src/components/ContactElement.svelte
@@ -1,13 +1,12 @@
 <script>
-  const { href, icon, name, className, children } = $props();
+  const { href, name, className, children } = $props();
 </script>
 
 <a
   class="h-8 w-full min-w-48 btn relative btn-primary text-base-content border-transparent shadow-md {className}"
   {href}
-  target="_blank"
->
-  <img alt="{icon} image" class="absolute right-5 h-5 text-white" src="/images/{icon}.svg" />
+  target="_blank">
+  {@render children()}
   <p class="absolute text-l left-5">{name}</p>
 </a>
 

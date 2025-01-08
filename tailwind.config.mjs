f28691bb7717e500153f1899a14a8940b8917fc4
April Hall <ari@arithefirst.com>
2025-01-08 18:40:50 -0500

    Perfect the icon sizing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,7 +24,10 @@ export default {
       },
       transitionTimingFunction: {
         'out-back': 'cubic-bezier( 0.175, 0.885, 0.32, 1.275 )'
-      }
+      },
+      borderRadius: {
+        'icon': '26.666%'
+      },
     },
   },
 };

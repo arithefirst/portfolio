2a4d80ea4794e39b592c82faab392500c18552e0
April Hall <ari@arithefirst.com>
2025-01-31 09:42:22 -0500

    feat: Process projects dynamically

---
+++ src/components/projects.ts
@@ -0,0 +1,42 @@
+import type { project } from './types';
+const projects: project[] = [
+  {
+    title: 'Kōshi',
+    altname: 'koshi',
+    desc: 'an Imageboard software designed for hikari3.ch',
+    images: [
+      {
+        src: '',
+        alt: '',
+      },
+      {
+        src: '',
+        alt: '',
+      },
+    ],
+    badges: [
+      {
+        content: 'CSS',
+        url: '',
+      },
+      {
+        content: 'EJS',
+        url: '',
+      },
+      {
+        content: 'Express',
+        url: '',
+      },
+      {
+        content: 'JavaScript',
+        url: '',
+      },
+      {
+        content: 'MongoDB',
+        url: '',
+      },
+    ],
+  },
+];
+
+export default projects;

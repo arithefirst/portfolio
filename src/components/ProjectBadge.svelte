e267d3f05a4c5e3468484c8912583eaa7461f8cd
April Hall <ari@arithefirst.com>
2025-01-31 14:19:42 -0500

    fix: Allow tags to wrap

--- src/components/ProjectBadge.svelte
+++ src/components/ProjectBadge.svelte
@@ -8,9 +8,9 @@
   const { badges }: Props = $props();
 </script>
 
-<div class="flex gap-1 w-1/4 text-center mt-1">
+<div class="flex gap-1 w-full text-center mt-1 flex-wrap">
   {#each badges as tag}
-    <a class="rounded-lg px-2 border-base-content border basis-1/4" target="_blank" rel="noreferrer" href={tag.url}
+    <a class="rounded-lg px-2 border-base-content border w-fit" target="_blank" rel="noreferrer" href={tag.url}
       >{tag.content}</a
     >
   {/each}

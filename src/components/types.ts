4815f29f54003ec81ca126cce1f6bf9d3a8c1fc9
April Hall <ari@arithefirst.com>
2025-01-31 09:42:22 -0500

    feat: Process projects dynamically

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,10 +1,18 @@
 export interface image {
-    src: string;
-    alt: string;
-    class?: string;
+  src: string;
+  alt: string;
+  class?: string;
 }
 
 export interface badge {
-    content: string;
-    url: string;
+  content: string;
+  url: string;
+}
+
+export interface project {
+  title: string;
+  altname?: string;
+  desc: string;
+  images: image[];
+  badges: badge[];
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [daisyui],
   daisyui: {
     themes: [
       {
         dark: {
-          accent: "#4ab090",
-          secondary: "#1e6c53",
-          primary: "#179b71",
-          "base-100": "#1d232a",
-          "base-content": "#f5f5f6",
+          accent: '#4ab090',
+          secondary: '#1e6c53',
+          primary: '#179b71',
+          'base-100': '#1d232a',
+          'base-content': '#f5f5f6',
         },
       },
     ],
@@ -20,17 +20,17 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        "legacy-img": "0 0.5rem 0.5rem 0 rgba(38, 37, 37, 0.541)",
+        'legacy-img': '0 0.5rem 0.5rem 0 rgba(38, 37, 37, 0.541)',
       },
       transitionTimingFunction: {
-        "out-back": "cubic-bezier( 0.175, 0.885, 0.32, 1.275 )",
+        'out-back': 'cubic-bezier( 0.175, 0.885, 0.32, 1.275 )',
       },
       borderRadius: {
-        icon: "26.666%",
+        icon: '26.666%',
       },
       colors: {
-        nav: "#191E242C",
-        "nav-border": "#191E241a",
+        nav: '#191E242C',
+        'nav-border': '#191E241a',
       },
     },
   },
